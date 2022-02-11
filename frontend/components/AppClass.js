84f681ae6242cd26a1586b263a430bc1c2525e4b
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-02-11 13:39:25 -0700

    ok email imput and submit button works thats probably all i can figure out

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -12,29 +12,46 @@ export default class AppClass extends React.Component {
     x: 2,
     y: 2,
     email: "lady@gaga.com",
+    message: "",
   };
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   axios
+  //     .post(`http://localhost:9000/api/result`, this.state)
+  //     .then((resp) => {
+  //       console.log(resp);
+  //       this.setState({ ...this.state, message: resp.data.message });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+
+  // handleLeftClick = (e) => {
+  //   e.preventDefault();
+  //   this.setState({
+  //     ...this.state,
+  //   });
+  // };
+  // }
+
+  onChange = (event) => {
+    const { value, id } = event.target;
+    this.setState({ ...this.state, [id]: value });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+
     axios
-      .post(`http://localhost:9000/api/result`, this.state.email)
-      .then((resp) => {
-        console.log(resp);
-        // this.setState({
-        //    ...this.state,
-        //    email: resp.data.message
-        // })
+      .post(`http://localhost:9000/api/result`, this.state)
+      .then((res) => {
+        console.log("here: ", res);
+        this.setState({ ...this.state, message: res.data.message });
       })
       .catch((err) => {
         console.log(err);
       });
-
-    // handleLeftClick = (e) => {
-    //   e.preventDefault();
-    //   this.setState({
-    //     ...this.state,
-    //   });
-    // };
-  }
+  };
 
   render() {
     console.log(this.state);
@@ -42,8 +59,8 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates ({className.coordinates})</h3>
-          <h3 id="steps">You moved {className.steps} times</h3>
+          <h3 id="coordinates">Coordinates ({this.state.coordinates})</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
           {this.state.matrix.map((row) => {
@@ -66,7 +83,7 @@ export default class AppClass extends React.Component {
         </div>
 
         <div className="info">
-          <h3 id="message">{}</h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
           <button onClick={this.handleLeftClick} id="left">
@@ -77,8 +94,14 @@ export default class AppClass extends React.Component {
           <button id="down">DOWN</button>
           <button id="reset">reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={this.onSubmit}>
+          <input
+            onChange={this.onChange}
+            id="email"
+            type="email"
+            value={this.state.value}
+            placeholder="type email"
+          ></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
